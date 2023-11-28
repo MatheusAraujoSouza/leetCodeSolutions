4fffa2dfae7d5a7c5157a6cf25a5bf9958dbbb9b
Matheus Araujo Souza <mhatheussss@gmail.com>
2023-11-27 22:46:07 -0300

    adding one more solution

---
+++ Problems/Problems/Array/TwoSum.cs
@@ -0,0 +1,31 @@
+﻿
+//Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+
+//You may assume that each input would have exactly one solution, and you may not use the same element twice.
+
+//You can return the answer in any order.
+
+namespace Problems.Problems.Array
+{
+    public class Solution
+    {
+        public int[] TwoSum(int[] nums, int target)
+        {
+            var result = new int[2];
+            for (int i = 1; i < nums.Length; i++)
+            {
+                for (int j = 0; j < i; j++)
+                {
+                    if (nums[i] + nums[j] == target)
+                    {
+                        result[0] = j;
+                        result[1] = i;
+                        return result;
+                    }
+                }   
+            }
+            return result;
+
+        }
+    }
+}
